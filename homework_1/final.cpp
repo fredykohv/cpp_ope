c5c5ed75cf43531556370b6481b5855f81450a1a
Fredy Kohv <fredykohv14@gmail.com>
2020-09-15 17:30:29 +0300

    All tasks functions in one file and executable to run all
    All tasks are compiled into one executable './homework'

---
+++ homework_1/final.cpp
@@ -0,0 +1,14 @@
+#include "circle_square.hpp"
+#include "salaries.hpp"
+#include "temperatures.hpp"
+
+#include <iostream>
+
+int main()
+{
+    circle_square::main_square();
+
+    salaries::main_salaries();
+
+    temperatures::main_temperatures();
+}
