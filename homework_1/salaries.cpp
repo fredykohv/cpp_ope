683f386564d94658e2c84f56a819bade956ec5c6
Fredy Kohv <fredykohv14@gmail.com>
2020-09-17 21:17:42 +0300

    Changed function type

--- homework_1/salaries.cpp
+++ homework_1/salaries.cpp
@@ -5,17 +5,21 @@ namespace salaries
 {
 
 float eligible_salary_for_wage_increase;
+float new_salary;
+float wage_increase_percentage;
+
 
 std::vector<float> salaries{1262.34, 871.21, 724.12, 2851.11, 2895.04, 125.3, 815.12, 1873.19};
 
-int wage_increase()
+float wage_increase()
 {
+    wage_increase_percentage = wage_increase_percentage / 100;
     for (int i = 0; i < salaries.size(); i++)
     {
         if (salaries[i] <= eligible_salary_for_wage_increase)
         {
             std::cout << "Your salary was: " << salaries[i] << " and you are eligible for a wage increase!" << std::endl;
-            float new_salary = salaries[i] + (salaries[i] * 0.05);
+            new_salary = salaries[i] + (salaries[i] * wage_increase_percentage);
             std::cout << "Your new salary is: " << new_salary << std::endl;
         }
     }
@@ -27,6 +31,8 @@ int main_salaries()
 {
     std::cout << "Set your eligible salary size in order to give your workers a 5% wage increase who earn less than your eligible salary size!" << std::endl;
     std::cin >> eligible_salary_for_wage_increase;
+    std::cout << "Set your wage increase percentage!" << std::endl;
+    std::cin >> wage_increase_percentage;
     
     wage_increase();
 
